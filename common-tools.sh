023be54dc452db8a495abef54352fb4a93827ccd
dappnodedev <diego@dappnode.io>
2024-07-01 17:09:14 +0200

    Add common tools

---
+++ common-tools.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Verify if the current NETWORK is supported
+#
+# Arguments:
+#   $1: A space-separated list of supported networks
+verify_network_support() {
+    supported_networks=$1 # List of supported networks
+
+    if [ -z "$NETWORK" ]; then
+        echo "[ERROR - entrypoint] NETWORK is not set"
+        exit 1
+    fi
+
+    for supported_network in $supported_networks; do
+        if [ "$supported_network" = "$NETWORK" ]; then
+            echo "[INFO - entrypoint] NETWORK is set to $NETWORK"
+            return 0
+        fi
+    done
+
+    echo "[ERROR - entrypoint] NETWORK $NETWORK is not supported"
+    exit 1
+}
